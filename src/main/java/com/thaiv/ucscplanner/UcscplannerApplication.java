be7f381314c6b2de93c9c578b2ec1b2d0c7f54cf
Thai Villaluna <thaivillaluna@gmail.com>
2023-08-21 12:36:01 -0700

    First commit

---
+++ src/main/java/com/thaiv/ucscplanner/UcscplannerApplication.java
@@ -0,0 +1,13 @@
+package com.thaiv.ucscplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcscplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcscplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thaiv/ucscplanner/UcscplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaiv.ucscplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcscplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
