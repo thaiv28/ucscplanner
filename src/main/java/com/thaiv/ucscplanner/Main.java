1c189b2459c238e0f08e4ab91aebf55aba9b69bc
Thai Villaluna <thaivillaluna@gmail.com>
2024-04-03 18:43:24 -0700

    add scan for repository/model in coursedb

--- src/main/java/com/thaiv/ucscplanner/Main.java
+++ src/main/java/com/thaiv/ucscplanner/Main.java
@@ -2,9 +2,17 @@ package com.thaiv.ucscplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
 
 
 @SpringBootApplication
+@EnableJpaRepositories("com.thaiv.coursedb.repositories")
+@EntityScan("com.thaiv.coursedb.models")
 public class Main {
 
 	public static void main(String[] args) {
