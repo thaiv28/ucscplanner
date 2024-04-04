dc3248c34f8f39d27cddfbdbe3f067646bf19b9b
Thai Villaluna <thaivillaluna@gmail.com>
2024-04-03 18:44:13 -0700

    remove unecessary imports

--- src/main/java/com/thaiv/ucscplanner/Main.java
+++ src/main/java/com/thaiv/ucscplanner/Main.java
@@ -3,12 +3,7 @@ package com.thaiv.ucscplanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
-
-import jakarta.persistence.Entity;
-
 
 @SpringBootApplication
 @EnableJpaRepositories("com.thaiv.coursedb.repositories")
