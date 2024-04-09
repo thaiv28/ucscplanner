0f6cc190c8df7cbecc2ec5fa8b2fadb06fabb442
Thai Villaluna <thaivillaluna@gmail.com>
2024-04-08 19:41:09 -0700

    feat: test infix to postfix

--- src/test/java/com/thaiv/plansc/ucscplanner/services/PreqServiceTest.java
+++ src/test/java/com/thaiv/plansc/ucscplanner/services/PreqServiceTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.stereotype.Service;
 
+import com.thaiv.plansc.coursedb.models.Course;
 import com.thaiv.plansc.coursedb.repositories.CourseRepository;
 import com.thaiv.plansc.ucscplanner.services.CourseService;
 import com.thaiv.plansc.ucscplanner.services.PreqPostfixService;
@@ -29,7 +31,7 @@ public class PreqServiceTest {
     void initService(){
         CourseService courseService = new CourseService(courseRepository);
         preqService = new PreqService(courseService);
-        preqPostfixService = new PreqPostfixService(courseService);
+        preqPostfixService = new PreqPostfixService();
     }
 
     @Test
@@ -38,8 +40,10 @@ public class PreqServiceTest {
         "19A; or AM 10 and MATH 20A; or MATH 21 and MATH 11A; or MATH 21 and " +
         "MATH 19A; or MATH 21 and MATH 20A.";
 
-        String replaced = preqPostfixService.replaceOperators(preq);
+        String replaced = preqPostfixService.cleanString(preq);
         System.out.println(replaced);
+        System.out.println("Postfix: " + preqPostfixService.convertToPostfix(preq));
+
     }
 
 }
